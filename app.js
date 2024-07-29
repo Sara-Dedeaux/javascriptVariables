b3575cb21ace28fbe498c49a222a6ba171a65037
Sara-Dedeaux <sara.dedeaux@mscoding.org>
2024-07-29 15:07:40 -0500

    exercises completed

--- app.js
+++ app.js
@@ -55,3 +55,116 @@ age= currYear-birthYear;
 //WRITE TO CONSOLE
 console.log(age);
 
+
+//INTRO TO CONST (CONSTANTS)-- IMMUTABLE
+
+//DECLARE CONST 
+const bar = "You can not change me!";
+
+// //ATTEMPT TO CHANGE CONST
+// bar=8;
+
+//WRITE TO CONSOLE
+console.log(bar);
+
+
+//INTRO TO LET -- ACCESSIBLE BASED ON SCOPE
+
+//DECLARE LET VARIABLE
+let bar2= "You can change me!";
+
+//CHANGE LET VARIABLE
+bar2=10;
+
+//WRITE TO CONSOLE
+console.log(bar2);
+
+
+//ADDRESSING QUOTATION MARKS INSIDE A STRING 
+
+//DOUBLE QUOTES OUTSIDE SINGLE QUOTE INSIDE
+let stringQuote= "You aren't special";
+console.log(stringQuote);
+
+//FORWARD SLASH FOR ESCAPE KEY
+let escapeQuote= 'You aren\'t special';
+console.log(escapeQuote);
+
+
+//ITERATING NUMBERS INCREMENT AND DECREMENT
+
+//DECLARE VARIABLE 
+let num3=0;
+console.log(num3);
+
+//ITERATE UP - INCREMENT
+num3++;
+console.log(num3);
+
+//ITERATE DOWN - DECREMENT
+num3--;
+console.log(num3);
+
+
+//TEMPLATE LITERALS- USE BUTTON ABOVE TAB TO NEST STRING-- NOT QUOTATION MARKS--
+console.log(`Hello Baz2 ${num3}`);
+
+
+//THIRD EXERCISE - PERSONAL GREETING
+    //INPUT THE USER'S NAME
+    //PERSONALIZED GREETING MESSAGE
+
+//DECLARE VARIABLES
+let greeting="Good Afternoon,";
+let name1="Sara";
+let nameGreet;
+
+//CALCULATIONS
+nameGreet= greeting + name1
+
+//WRITE TO CONSOLE
+console.log(nameGreet);
+
+
+//FOURTH EXERCISE - TIP CALCULATOR
+    //INPUT THE USER'S BILL AMOUNT AND THE TIP PERCENTAGE
+    //CALCULATE THE TIP AMOUNT AND CONSOLE THE TOTAL BILL.
+
+//DECLARE VARIABLES
+let userBill=25.00;
+let tipPerc=.25;
+let tipTotal; 
+let totalCost;
+
+//CALCULATIONS
+
+//TIP calculation
+tipTotal= userBill*tipPerc;
+
+//TOTAL calculation
+totalCost=tipTotal+userBill;
+
+//WRITE TO CONSOLE
+//variable add .toFixed(2) -- used to round variable tWo decimal places
+console.log(`$${totalCost.toFixed(2)}`);
+
+
+//FIFTH EXERCISE - AGE CALCULATOR IN DAYS
+    //ASK THE USER TO INPUT THEIR AGE IN YEARS.
+    //MULTIPLY THE AGE IN YEARS BY 365
+
+//DECLARE VARIABLES
+let userAge=10;
+let ageInDays;
+
+//CALCULATIONS
+ageInDays=userAge*365;
+
+//WRITE TO CONSOLE
+console.log(` A person ${userAge} year(s) old is ${ageInDays} days old.`);
+
+
+
+
+
+

8d78160b54368624cefb01be6e618b0b57b849f6
Sara-Dedeaux <sara.dedeaux@mscoding.org>
2024-07-29 12:50:14 -0500

    Variable Practice
    temp conversion and
    age calculator

---
+++ app.js
@@ -0,0 +1,57 @@
+//DECLARE VARIABLES
+var hello = "world";
+var num = 7;
+var bool = true;
+var undef = undefined;
+var none = null; 
+
+//CHANGE VARIABLE DATA- DATA-TYPE CAN BE CHANGED AS WELL 
+num=29;
+num="17"; 
+
+
+//WRTIE TO CONSOLE
+console.log(5);
+console.log("World");
+console.log(hello);
+console.log(num);
+console.log(bool);
+console.log(undef);
+console.log(none);
+
+
+//MATH VARIABLES 
+var foo; 
+foo=19;
+console.log(foo + 4);
+
+var textOne="Hello";
+console.log(textOne+" World!");
+
+
+//FIRST EXERCISE - TEMP. CONVERTER -- CONVERT FARENHEIT TO CELSUIS 
+
+//DECLARE VARIABLES
+var fah;
+var cel=0;
+
+//CALCULATIONS
+fah=cel * 9/5 + 32;
+
+//WRITE TO CONSOLE
+console.log(fah);
+
+
+//SECOND EXERCISE - AGE CALCULATOR -- SUBTRACT BIRTH YEAR FROM CURRENT YEAR TO GET AGE
+
+//DECLARE VARIABLES
+var currYear=2024;
+var birthYear=1989;
+var age;
+
+//CALCULATIONS
+age= currYear-birthYear;
+
+//WRITE TO CONSOLE
+console.log(age);
+
